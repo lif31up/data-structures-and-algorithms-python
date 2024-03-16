bebd6177e102a64f700e1ecaedd483319513dad3
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-03-16 17:11:24 +0900

    feature: add LinkedList

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -12,7 +12,6 @@ class LinkedList:
   def __init__(self, match, destroy):
     self.match = match
     self.destroy = destroy
-  # __init__():
 
   def ins_next(self, node: Node, data) -> int:
     new_node = Node(data)
@@ -21,15 +20,25 @@ class LinkedList:
     node.next = new_node
     self.size += 1
     return 0
-  # __init__():
 
   def rem_next(self, node: Node) -> Node and None:
-    old_node = node.next
     if self.size == 0: return None
-    if old_node.next is None: self.tail = None
-    node.next = old_node.next
-    self.size -= 1
-    return self.destroy(old_node)
-  # rem_next():
+    if node == None:
+      old_node = self.head
+      self.head = self.head.next
+      self.size -= 1
+      if self.size == 1: self.tail = self.head
+      return self.destroy(old_node)
+    else:
+      if node == None: return -1
+      old_node = node.next
+      node.next = old_node.next
+      if node.next == None: self.tail = node.next
+      self.size -= 1
+      return self.destroy(old_node)
 
+  def getPenultimate(self) -> Node:
+    curr = self.head
+    while curr.next is not None: curr = curr.next
+    return curr
 # LinkedList
