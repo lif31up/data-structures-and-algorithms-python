aa00cd3e6683ffd9ce4d259324c0fe93cddf232c
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-03-19 17:00:00 +0900

    fix: exclude class def

---
+++ BinaryTree/Node.py
@@ -0,0 +1,7 @@
+class Node:
+  key: int
+  data: object
+  left: object
+  right: object
+
+  def __init__(self, data): self.data = data
---
+++ LinkedList/Node.py
@@ -0,0 +1,5 @@
+class Node:
+  data: object
+  next: object
+
+  def __init__(self, data): self.data = data
